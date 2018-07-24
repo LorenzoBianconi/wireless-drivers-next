1aa017ffff11040365920e567074642339b9f049
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-07-24 11:00:07 +0200

    mt76: move mt76x2u_remove_dma_hdr in mt76xx-lib module
    Move mt76x2u_remove_dma_hdr in mt76xx-lib module and rename it in
    mt76xx_remove_dma_hdr. Moreover use mt76xx_remove_hdr_pad routine
    in mt76xx_remove_dma_hdr function.
    Furthermore remove mt76x0_tx_skb_remove_dma_overhead routine

--- drivers/net/wireless/mediatek/mt76/mt76xx_util.h
+++ drivers/net/wireless/mediatek/mt76/mt76xx_util.h
@@ -46,4 +46,5 @@ void mt76xx_sta_rate_tbl_update(struct ieee80211_hw *hw,
 int mt76xx_insert_hdr_pad(struct sk_buff *skb);
 void mt76xx_remove_hdr_pad(struct sk_buff *skb, int len);
 void mt76xx_tx_complete(struct mt76_dev *dev, struct sk_buff *skb);
+void mt76xx_remove_dma_hdr(struct sk_buff *skb);
 #endif
