706e2bc72d6095592be15503e42069da7ae33b4e
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-07-28 12:12:29 +0200

    mt76x2u: update fw file name

--- drivers/net/wireless/mediatek/mt76/mt76x2.h
+++ drivers/net/wireless/mediatek/mt76/mt76x2.h
@@ -32,8 +32,8 @@
 #define MT7662_ROM_PATCH	"mt7662_rom_patch.bin"
 #define MT7662_EEPROM_SIZE	512
 
-#define MT7662U_FIRMWARE	"mt7662u.bin"
-#define MT7662U_ROM_PATCH	"mt7662u_rom_patch.bin"
+#define MT7662U_FIRMWARE	"mediatek/mt7662u.bin"
+#define MT7662U_ROM_PATCH	"mediatek/mt7662u_rom_patch.bin"
 
 #define MT76x2_RX_RING_SIZE	256
 #define MT_RX_HEADROOM		32
