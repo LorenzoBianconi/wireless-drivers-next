9fce34c5d871ea79ec14859a1c24b49dff16029b
Lorenzo Bianconi <lorenzo@kernel.org>
2019-11-04 12:03:41 +0100

    mt76: generalize mt76_mcu_rx_event

--- drivers/net/wireless/mediatek/mt76/mt7603/dma.c
+++ drivers/net/wireless/mediatek/mt76/mt7603/dma.c
@@ -92,7 +92,7 @@ void mt7603_queue_rx_skb(struct mt76_dev *mdev, enum mt76_rxq_id q,
 
 	if (q == MT_RXQ_MCU) {
 		if (type == PKT_TYPE_RX_EVENT)
-			mt76_mcu_rx_event(&dev->mt76, skb);
+			mt76_mcu_rx_event(&dev->mt76.mcu, skb);
 		else
 			mt7603_rx_loopback_skb(dev, skb);
 		return;
@@ -105,7 +105,7 @@ void mt7603_queue_rx_skb(struct mt76_dev *mdev, enum mt76_rxq_id q,
 		dev_kfree_skb(skb);
 		break;
 	case PKT_TYPE_RX_EVENT:
-		mt76_mcu_rx_event(&dev->mt76, skb);
+		mt76_mcu_rx_event(&dev->mt76.mcu, skb);
 		return;
 	case PKT_TYPE_NORMAL:
 		if (mt7603_mac_fill_rx(dev, skb) == 0) {
