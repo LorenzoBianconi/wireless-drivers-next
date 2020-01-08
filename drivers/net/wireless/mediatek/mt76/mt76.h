0bdf97c5113ba238acd165d852975b22291b0981
Sean Wang <sean.wang@mediatek.com>
2020-01-08 08:11:14 +0800

    mt76u: extend scatter gatther number on Rx
    Extending scatter gather number to 4 on Rx allows to urb fit in whole AMSDU
    with the maximum size 11,454.
    
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -396,7 +396,7 @@ struct mt76_mcu {
 };
 
 #define MT_TX_SG_MAX_SIZE	8
-#define MT_RX_SG_MAX_SIZE	1
+#define MT_RX_SG_MAX_SIZE	4
 #define MT_NUM_TX_ENTRIES	256
 #define MT_NUM_RX_ENTRIES	128
 #define MCU_RESP_URB_SIZE	1024
