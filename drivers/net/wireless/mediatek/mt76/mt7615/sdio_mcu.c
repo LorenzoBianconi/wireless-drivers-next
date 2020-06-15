6c394eb4029aec06d201d555cb2c4e97e3d45d25
Sean Wang <sean.wang@mediatek.com>
2020-06-15 14:18:35 -0700

    mt76: add mcu_rr and mcu_wr to mcu_ops
    Use mcu to perform registers access with mcu_rr/wr
    
    Co-developed-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt7615/sdio_mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7615/sdio_mcu.c
@@ -61,6 +61,8 @@ int mt7663s_mcu_init(struct mt7615_dev *dev)
 		.mcu_skb_send_msg = mt7663s_mcu_send_message,
 		.mcu_send_msg = mt7615_mcu_msg_send,
 		.mcu_restart = mt7615_mcu_restart,
+		.mcu_rr = mt7615_mcu_reg_rr,
+		.mcu_wr = mt7615_mcu_reg_wr,
 	};
 	int ret;
 
