4eee9037bf259f953c5c633c8740f91e6f45a1ce
Lorenzo Bianconi <lorenzo@kernel.org>
2020-07-17 10:15:17 +0200

    mt76: mt7663s: move tx/rx processing in a dedicated kthread - WIP 2

--- drivers/net/wireless/mediatek/mt76/mt7615/sdio_txrx.c
+++ drivers/net/wireless/mediatek/mt76/mt7615/sdio_txrx.c
@@ -86,7 +86,10 @@ static int mt7663s_rx_run_queue(struct mt7615_dev *dev, enum mt76_rxq_id qid,
 
 	buf = page_address(page);
 
+	sdio_claim_host(sdio->func);
 	err = sdio_readsb(sdio->func, buf, MCR_WRDR(qid), len);
+	sdio_release_host(sdio->func);
+
 	if (err < 0) {
 		dev_err(dev->mt76.dev, "sdio read data failed:%d\n", err);
 		__free_pages(page, order);
@@ -174,7 +177,10 @@ static int mt7663s_tx_run_queue(struct mt7615_dev *dev, struct mt76_queue *q)
 			len = roundup(len, sdio->func->cur_blksize);
 
 		/* TODO: skb_walk_frags and then write to SDIO port */
+		sdio_claim_host(sdio->func);
 		err = sdio_writesb(sdio->func, MCR_WTDR1, e->skb->data, len);
+		sdio_release_host(sdio->func);
+
 		if (err) {
 			dev_err(dev->mt76.dev, "sdio write failed: %d\n", err);
 			return -EIO;
@@ -188,6 +194,23 @@ static int mt7663s_tx_run_queue(struct mt7615_dev *dev, struct mt76_queue *q)
 	return nframes;
 }
 
+static int mt7663s_tx_run_queues(struct mt7615_dev *dev)
+{
+	int i, nframes = 0;
+
+	for (i = 0; i < MT_TXQ_MCU_WA; i++) {
+		int ret;
+
+		ret = mt7663s_tx_run_queue(dev, dev->mt76.q_tx[i].q);
+		if (ret < 0)
+			continue;
+
+		nframes += ret;
+	}
+
+	return nframes;
+}
+
 int mt7663s_kthread_run(void *data)
 {
 	struct mt7615_dev *dev = data;
@@ -195,35 +218,41 @@ int mt7663s_kthread_run(void *data)
 
 	while (!kthread_should_stop()) {
 		struct mt76s_intr intr;
-		int i;
 
 		cond_resched();
 
 		sdio_claim_host(sdio->func);
 		sdio_writel(sdio->func, WHLPCR_INT_EN_CLR, MCR_WHLPCR, 0);
+		sdio_release_host(sdio->func);
 
-		while (true) {
-			for (i = 0; i < MT_TXQ_MCU_WA; i++)
-				mt7663s_tx_run_queue(dev, dev->mt76.q_tx[i].q);
+		mt7663s_tx_run_queues(dev);
 
+		do {
+			sdio_claim_host(sdio->func);
 			sdio_readsb(sdio->func, &intr, MCR_WHISR,
 				    sizeof(struct mt76s_intr));
+			sdio_release_host(sdio->func);
+
 			trace_dev_irq(&dev->mt76, intr.isr, 0);
 
-			if (!intr.isr)
-				break;
+			if (intr.isr & WHIER_RX0_DONE_INT_EN)
+				mt7663s_rx_run_queue(dev, 0, &intr);
 
-			mt7663s_refill_sched_quota(dev, intr.tx.wtqcr);
-			for (i = 0; i < MT_TXQ_MCU_WA; i++)
-				mt7663s_tx_run_queue(dev, dev->mt76.q_tx[i].q);
+			if (intr.isr & WHIER_RX1_DONE_INT_EN)
+				mt7663s_rx_run_queue(dev, 1, &intr);
 
-			mt76_for_each_q_rx(&dev->mt76, i)
-				mt7663s_rx_run_queue(dev, i, &intr);
+			if (intr.isr & WHIER_TX_DONE_INT_EN) {
+				mt7663s_refill_sched_quota(dev, intr.tx.wtqcr);
+				mt7663s_tx_run_queues(dev);
+			}
 
-			set_bit(MT76S_STATE_STATUS_PENDING, &sdio->state);
-			wake_up(&sdio->status_wait);
-		}
+			if (intr.isr & WHIER_TXRX_DONE_INT_EN) {
+				set_bit(MT76S_STATE_STATUS_PENDING, &sdio->state);
+				wake_up(&sdio->status_wait);
+			}
+		} while (intr.isr);
 
+		sdio_claim_host(sdio->func);
 		sdio_writel(sdio->func, WHLPCR_INT_EN_SET, MCR_WHLPCR, 0);
 		sdio_release_host(sdio->func);
 
