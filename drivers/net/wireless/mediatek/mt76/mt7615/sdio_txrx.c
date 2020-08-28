803e2197e0583fd1a47b67e03d9371ee31e3fc32
Sean Wang <objelf@gmail.com>
2020-08-27 20:10:05 -0700

    mt76: mt7663s: consider fractional pse quota

--- drivers/net/wireless/mediatek/mt76/mt7615/sdio_txrx.c
+++ drivers/net/wireless/mediatek/mt76/mt7615/sdio_txrx.c
@@ -32,8 +32,10 @@ mt7663s_update_queue(struct mt76_dev *dev, enum mt76_txq_id qid,
 
 		size = DIV_ROUND_UP(e->buf_sz + sdio->sched.deficit,
 				    MT_PSE_PAGE_SZ);
-		if (quota < size)
+		if (quota < size) {
+			e->buf_sz -= MT_PSE_PAGE_SZ * quota;
 			break;
+		}
 
 		q->last = (q->last + 1) % q->ndesc;
 		e->done = true;
