7bf4f69e78ee05508cc6a8b69b17c29b1889374a
Sean Wang <objelf@gmail.com>
2021-05-01 00:22:13 -0700

    mt76: mt7921: fix UC entry is being overwritten
    Fix UC entry is being overwritten by BC entry
    
    Fixes: 36fcc8cff592 ("mt76: mt7921: introduce mt7921_mcu_sta_add routine")
    Signed-off-by: Deren Wu <deren.wu@mediatek.com>
    Signed-off-by: YN Chen <yn.chen@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt76_connac_mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt76_connac_mcu.c
@@ -837,10 +837,13 @@ int mt76_connac_mcu_add_sta_cmd(struct mt76_phy *phy,
 	if (IS_ERR(skb))
 		return PTR_ERR(skb);
 
-	mt76_connac_mcu_sta_basic_tlv(skb, info->vif, info->sta, info->enable);
-	if (info->enable && info->sta)
-		mt76_connac_mcu_sta_tlv(phy, skb, info->sta, info->vif,
-					info->rcpi);
+	if (info->sta) {
+		mt76_connac_mcu_sta_basic_tlv(skb, info->vif, info->sta,
+					      info->enable);
+		if (info->enable && info->sta)
+			mt76_connac_mcu_sta_tlv(phy, skb, info->sta,
+						info->vif, info->rcpi);
+	}
 
 	sta_wtbl = mt76_connac_mcu_add_tlv(skb, STA_REC_WTBL,
 					   sizeof(struct tlv));
