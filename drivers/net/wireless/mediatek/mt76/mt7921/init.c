274fb3d41fe8a9d8e29bb5cc56ec8e9909c51af6
Lorenzo Bianconi <lorenzo@kernel.org>
2021-05-05 15:14:48 +0200

    mt76: mt7921: enable rx csum offload

--- drivers/net/wireless/mediatek/mt76/mt7921/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/init.c
@@ -51,6 +51,7 @@ mt7921_init_wiphy(struct ieee80211_hw *hw)
 	hw->queues = 4;
 	hw->max_rx_aggregation_subframes = IEEE80211_MAX_AMPDU_BUF;
 	hw->max_tx_aggregation_subframes = IEEE80211_MAX_AMPDU_BUF;
+	hw->netdev_features = NETIF_F_RXCSUM;
 
 	hw->radiotap_timestamp.units_pos =
 		IEEE80211_RADIOTAP_TIMESTAMP_UNIT_US;
